5d646c890ce6628741ea19845f9d51f1165397fb
J. Wąsala <s1620444@duranium.liacs.nl>
2022-01-11 13:37:29 +0100

    some restructuring of folders

---
+++ experiments/bert_preprocessing.py
@@ -0,0 +1,111 @@
+import pickle
+
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.model_selection import train_test_split
+from transformers import BertTokenizer
+
+# Create a function to tokenize a set of texts
+
+
+def preprocessing_for_bert(data, MAX_LEN=64):
+    """Perform required preprocessing steps for pretrained BERT.
+    @param    data (np.array): Array of texts to be processed.
+    @return   input_ids (torch.Tensor): Tensor of token ids to be fed to a model.
+    @return   attention_masks (torch.Tensor): Tensor of indices specifying which
+                  tokens should be attended to by the model.
+    """
+    # Create empty lists to store outputs
+    input_ids = []
+    attention_masks = []
+
+    # For every sentence...
+    for sent in data:
+        # `encode_plus` will:
+        #    (1) Tokenize the sentence
+        #    (2) Add the `[CLS]` and `[SEP]` token to the start and end
+        #    (3) Truncate/Pad sentence to max length
+        #    (4) Map tokens to their IDs
+        #    (5) Create attention mask
+        #    (6) Return a dictionary of outputs
+        encoded_sent = tokenizer.encode_plus(
+            text=str(sent),  # Preprocess sentence
+            add_special_tokens=True,        # Add `[CLS]` and `[SEP]`
+            max_length=MAX_LEN,                  # Max length to truncate/pad
+            padding='max_length',         # Pad sentence to max length
+            # return_tensors='pt',           # Return PyTorch tensor
+            return_attention_mask=True      # Return attention mask
+        )
+
+        # Add the outputs to the lists
+        input_ids.append(encoded_sent.get('input_ids'))
+        attention_masks.append(encoded_sent.get('attention_mask'))
+
+    # Convert lists to tensors
+    input_ids = torch.tensor(input_ids)
+    attention_masks = torch.tensor(attention_masks)
+
+    return input_ids, attention_masks
+
+
+if __name__ == "__main__":
+    # Load the BERT tokenizer
+
+    MAX_LEN = 64
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+
+    train_data = pd.read_csv('./data/downloaded/twitter-train-clean.tsv',
+                             sep='\t', names=['label', 'text'], encoding="utf-8")
+    test_data = pd.read_csv('./data/downloaded/twitter-test-clean.tsv',
+                            sep='\t', names=['label', 'text'], encoding="utf-8")
+    train_data.dropna(inplace=True)
+    test_data.dropna(inplace=True)
+
+    # remove lines that are too long where something went wrong
+    # with reading into pandas
+    train_indices = train_data['text'].apply(lambda x: len(
+        tokenizer.encode(str(x), add_special_tokens=True)) < MAX_LEN)
+    train_data = train_data[train_indices]
+    test_indices = test_data['text'].apply(lambda x: len(
+        tokenizer.encode(str(x), add_special_tokens=True)) < MAX_LEN)
+    test_data = test_data[test_indices]
+
+    with open("data/bert_tokenized/bert_train_data.pkl", "wb") as f:
+        pickle.dump(train_data, f)
+    with open("data/bert_tokenized/bert_test_data.pkl", "wb") as f:
+        pickle.dump(test_data, f)
+
+    # encode
+    concat = np.concatenate([train_data.text.values, test_data.text.values])
+    encoded_tweets = [tokenizer.encode(
+        sent, add_special_tokens=True) for sent in concat]
+
+    # split train into train and val
+    x_train, x_val, y_train_str, y_val_str = train_test_split(
+        train_data.text.values, train_data.label.values, test_size=0.2, random_state=2020)
+
+    # map labels to int
+    # map labels to 0,1,2
+    map_sent2int = {"negative": 0, "neutral": 1, "positive": 2}
+    y_train = np.array([map_sent2int[label] for label in y_train_str])
+    y_val = np.array([map_sent2int[label] for label in y_val_str])
+
+    with open("data/bert_tokenized/bert_y_train.pkl", "wb") as f:
+        pickle.dump(y_train, f)
+    with open("data/bert_tokenized/bert_y_val.pkl", "wb") as f:
+        pickle.dump(y_val, f)
+
+    # tokenize
+    train_inputs, train_masks = preprocessing_for_bert(x_train)
+    val_inputs, val_masks = preprocessing_for_bert(x_val)
+
+    with open("data/bert_tokenized/bert_train_inputs.pkl", "wb") as f:
+        pickle.dump(train_inputs, f)
+    with open("data/bert_tokenized/bert_val_inputs.pkl", "wb") as f:
+        pickle.dump(val_inputs, f)
+    with open("data/bert_tokenized/bert_train_masks.pkl", "wb") as f:
+        pickle.dump(train_masks, f)
+    with open("data/bert_tokenized/bert_val_masks.pkl", "wb") as f:
+        pickle.dump(val_masks, f)
---
+++ experiments/bert_util.py
@@ -0,0 +1,285 @@
+import random
+import time
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler,
+                              TensorDataset)
+from transformers import AdamW, BertModel, get_linear_schedule_with_warmup
+
+
+def create_data_loaders(train_data, train_inputs, val_inputs, train_masks, val_masks, y_train, y_val):
+    # Convert other data types to torch.Tensor
+    train_labels = torch.tensor(y_train)
+    val_labels = torch.tensor(y_val)
+
+    # For fine-tuning BERT, the authors recommend a batch size of 16 or 32.
+    batch_size = 32
+
+    # Create the DataLoader for our training set
+    train_data = TensorDataset(train_inputs, train_masks, train_labels)
+    train_sampler = RandomSampler(train_data)
+    train_dataloader = DataLoader(
+        train_data, sampler=train_sampler, batch_size=batch_size)
+
+    # Create the DataLoader for our validation set
+    val_data = TensorDataset(val_inputs, val_masks, val_labels)
+    val_sampler = SequentialSampler(val_data)
+    val_dataloader = DataLoader(
+        val_data, sampler=val_sampler, batch_size=batch_size)
+
+    return train_dataloader, val_dataloader
+
+# Create the BertClassfier class
+
+
+class BertClassifier(nn.Module):
+    """Bert Model for Classification Tasks.
+    """
+
+    def __init__(self, freeze_bert=False, hidden_layer=True):
+        """
+        @param    bert: a BertModel object
+        @param    classifier: a torch.nn.Module classifier
+        @param    freeze_bert (bool): Set `False` to fine-tune the BERT model
+        """
+        super(BertClassifier, self).__init__()
+        # Specify hidden size of BERT, hidden size of our classifier, and number of labels
+        D_in, H, D_out = 768, 50, 3
+
+        # Instantiate BERT model
+
+        self.bert = BertModel.from_pretrained('bert-base-cased')
+
+        # Instantiate an one-layer feed-forward classifier
+        if hidden_layer:
+            self.classifier = nn.Sequential(
+                nn.Linear(D_in, H),
+                nn.ReLU(),
+                nn.Dropout(0.35),
+                nn.Linear(H, D_out)
+            )
+        else:
+            self.classifier = nn.Sequential(
+                nn.Dropout(0.35),
+                nn.Linear(D_in, D_out)
+            )
+
+        # Freeze the BERT model
+        if freeze_bert:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+
+    def forward(self, input_ids, attention_mask):
+        """
+        Feed input to BERT and the classifier to compute logits.
+        @param    input_ids (torch.Tensor): an input tensor with shape (batch_size,
+                      max_length)
+        @param    attention_mask (torch.Tensor): a tensor that hold attention mask
+                      information with shape (batch_size, max_length)
+        @return   logits (torch.Tensor): an output tensor with shape (batch_size,
+                      num_labels)
+        """
+        # Feed input to BERT
+        outputs = self.bert(input_ids=input_ids,
+                            attention_mask=attention_mask)
+
+        # Extract the last hidden state of the token `[CLS]` for classification task
+        last_hidden_state_cls = outputs[0][:, 0, :]
+
+        # Feed input to classifier to compute logits
+        logits = self.classifier(last_hidden_state_cls)
+
+        return logits
+
+# train_dataloader, val_dataloader = create_data_loaders()
+
+
+def initialize_model(device, train_dataloader, epochs=4, lr=5e-5, hidden_layer=True):
+    """Initialize the Bert Classifier, the optimizer and the learning rate scheduler.
+    """
+    # Instantiate Bert Classifier
+    bert_classifier = BertClassifier(
+        freeze_bert=False, hidden_layer=hidden_layer)
+
+    # Tell PyTorch to run the model on GPU
+    bert_classifier.to(device)
+
+    # Create the optimizer
+    optimizer = AdamW(bert_classifier.parameters(),
+                      lr=lr,    # Default learning rate
+                      eps=1e-8    # Default epsilon value
+                      )
+
+    # Total number of training steps
+    total_steps = len(train_dataloader) * epochs
+
+    # Set up the learning rate scheduler
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=0,  # Default value
+                                                num_training_steps=total_steps)
+    return bert_classifier, optimizer, scheduler
+
+
+def set_seed(seed_value=42):
+    """Set seed for reproducibility.
+    """
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
+
+
+def train(model, train_dataloader, device, loss_fn, optimizer, scheduler, val_dataloader=None, epochs=4, evaluation=False):
+    """Train the BertClassifier model.
+    """
+    # Start training loop
+    print("Start training...\n")
+    for epoch_i in range(epochs):
+        # =======================================
+        #               Training
+        # =======================================
+        # Print the header of the result table
+        # print(f"{'Epoch':^7} | {'Batch':^7} | {'Train Loss':^12} | {'Val Loss':^10} | {'Val Acc':^9} | {'Elapsed':^9}")
+        # print("-"*70)
+
+        # Measure the elapsed time of each epoch
+        t0_epoch, t0_batch = time.time(), time.time()
+
+        # Reset tracking variables at the beginning of each epoch
+        total_loss, batch_loss, batch_counts = 0, 0, 0
+
+        # Put the model into the training mode
+        model.train()
+
+        # For each batch of training data...
+        for step, batch in enumerate(train_dataloader):
+            batch_counts += 1
+            # Load batch to GPU
+            b_input_ids, b_attn_mask, b_labels = tuple(
+                t.to(device) for t in batch)
+
+            # Zero out any previously calculated gradients
+            model.zero_grad()
+
+            # Perform a forward pass. This will return logits.
+            logits = model(b_input_ids, b_attn_mask)
+
+            # Compute loss and accumulate the loss values
+            loss = loss_fn(logits, b_labels)
+            batch_loss += loss.item()
+            total_loss += loss.item()
+
+            # Perform a backward pass to calculate gradients
+            loss.backward()
+
+            # Clip the norm of the gradients to 1.0 to prevent "exploding gradients"
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+            # Update parameters and the learning rate
+            optimizer.step()
+            scheduler.step()
+
+            # Print the loss values and time elapsed for every 20 batches
+            if (step % 20 == 0 and step != 0) or (step == len(train_dataloader) - 1):
+                # Calculate time elapsed for 20 batches
+                time_elapsed = time.time() - t0_batch
+
+                # Print training results
+                # print(
+                #     f"{epoch_i + 1:^7} | {step:^7} | {batch_loss / batch_counts:^12.6f} | {'-':^10} | {'-':^9} | {time_elapsed:^9.2f}")
+
+                # Reset batch tracking variables
+                batch_loss, batch_counts = 0, 0
+                t0_batch = time.time()
+
+        # Calculate the average loss over the entire training data
+        avg_train_loss = total_loss / len(train_dataloader)
+
+        # =======================================
+        #               Evaluation
+        # =======================================
+    # After the completion of each training epoch, measure the model's performance
+    # on our validation set.
+    val_loss, val_accuracy = evaluate(
+        model, val_dataloader, device, loss_fn)
+    train_loss, train_accuracy = evaluate(
+        model, train_dataloader, device, loss_fn)
+
+    # Print performance over the entire training data
+    time_elapsed = time.time() - t0_epoch
+
+    print(f"{train_accuracy:^12.6f} | {val_accuracy:^9.2f} ")
+
+    print("Training complete!")
+    return train_accuracy, val_accuracy
+
+
+def evaluate(model, val_dataloader, device, loss_fn):
+    """After the completion of each training epoch, measure the model's performance
+    on our validation set.
+    """
+    # Put the model into the evaluation mode. The dropout layers are disabled during
+    # the test time.
+    model.eval()
+
+    # Tracking variables
+    val_accuracy = []
+    val_loss = []
+
+    # For each batch in our validation set...
+    for batch in val_dataloader:
+        # Load batch to GPU
+        b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
+
+        # Compute logits
+        with torch.no_grad():
+            logits = model(b_input_ids, b_attn_mask)
+
+        # Compute loss
+        loss = loss_fn(logits, b_labels)
+        val_loss.append(loss.item())
+
+        # Get the predictions
+        preds = torch.argmax(logits, dim=1).flatten()
+
+        # Calculate the accuracy rate
+        accuracy = (preds == b_labels).cpu().numpy().mean() * 100
+        val_accuracy.append(accuracy)
+
+    # Compute the average accuracy and loss over the validation set.
+    val_loss = np.mean(val_loss)
+    val_accuracy = np.mean(val_accuracy)
+
+    return val_loss, val_accuracy
+
+
+def bert_predict(model, test_dataloader, device):
+    """Perform a forward pass on the trained BERT model to predict probabilities
+    on the test set.
+    """
+    # Put the model into the evaluation mode. The dropout layers are disabled during
+    # the test time.
+    model.eval()
+
+    all_logits = []
+
+    # For each batch in our test set...
+    for batch in test_dataloader:
+        # Load batch to GPU
+        b_input_ids, b_attn_mask = tuple(t.to(device) for t in batch)[:2]
+
+        # Compute logits
+        with torch.no_grad():
+            logits = model(b_input_ids, b_attn_mask)
+        all_logits.append(logits)
+
+    # Concatenate logits from each batch
+    all_logits = torch.cat(all_logits, dim=0)
+
+    # Apply softmax to calculate probabilities
+    probs = F.softmax(all_logits, dim=1).cpu().numpy()
+
+    return probs
